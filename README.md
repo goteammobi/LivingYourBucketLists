eb68339b2a48d43541c124f8fe9abaab308fabf6
Randy Truong <rtruo001@ucr.edu>
2015-04-04 07:26:50 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Built in LA Hacks 2015
+
+=============
+Andy Hollist
+Harrison Chen
+Justin Khoo
+Michael Yoon
+Randy Truong
---
+++ public/javascript/getLifeLeftover.js
@@ -0,0 +1,42 @@
+var months = new Object();
+months['1'] = 'January'
+months['2'] = 'February';
+months['3'] = 'March';
+months['4'] = 'April';
+months['5'] = 'May';
+months['6'] = 'June';
+months['7'] = 'July';
+months['8'] = 'August';
+months['9'] = 'September';
+months['10'] = 'October';
+months['11'] = 'November';
+months['12'] = 'December';
+
+$("#button-life-leftover").click(function(event){
+    var birthdate = document.getElementById("input-date");
+    birthdate = birthdate.value;
+    dateOfBirth = birthdate.split("/");
+
+    var month = dateOfBirth[0];
+    var day = dateOfBirth[1];
+    var year = dateOfBirth[2];
+
+    var key = 'X-Mashape-Key';
+    var value = '93gwp0ihgImshqp2RZbairh6LEv4p1LfKgMjsnwsEAc4u6tbbn'
+
+    //a GET JSON call using the ajax function.
+    $.ajax({
+      type: 'GET',
+      beforeSend: function (request) {
+        request.setRequestHeader(key, value);
+      },
+      url: "https://life-left.p.mashape.com/time-left?birth=" + day + " " + months[month] + " " + year,
+      dataType: 'json',
+      success: function(json) {
+        console.log("Here is the response: " + JSON.stringify(json)); 
+        return [true, ""];
+      }
+    });
+  });
+
+

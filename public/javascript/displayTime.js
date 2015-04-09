eebfbb44ff27df402cf1d42748e12aad548da9f4
Randy Truong <rtruo001@ucr.edu>
2015-04-08 22:22:45 -0700

    Added the timing countdown

--- public/javascript/displayTime.js
+++ public/javascript/displayTime.js
@@ -1,7 +1,49 @@
 //callback is called in the ajax get request when a valid birthday is given. Called from the JSON key complete.
 callback = function() {
   if (ifRealBirthday == true) {
-    $('#leftover-time').fadeIn(1000);
+    $('#leftover-time').fadeIn(500);
     ifRealBirthday = false;
+    setTime();
   }
+}
+
+function setTime() {
+  $("#years-left").text(yearsLeft);
+  $("#months-left").text(monthsLeft);
+  $("#days-left").text(daysLeft);
+  $("#hours-left").text(hoursLeft);
+  $("#minutes-left").text(minutesLeft);
+  $("#seconds-left").text(secondsLeft);
+}
+
+var myVar = setInterval(function() {
+  myTimer()
+} , 1000);
+
+function myTimer() {
+  if (secondsLeft <= 0) {
+      if (minutesLeft <= 0) {
+          if (monthsLeft <= 0) {
+              if (yearsLeft <= 0) {
+                  //Out of time
+              }
+              else {
+                  --yearsLeft;
+              }
+              monthsLeft = 11;
+          }
+          else {
+              --monthsLeft;
+          }
+          minutesLeft = 59;
+      }
+      else {
+          --minutesLeft;
+      }
+      secondsLeft = 59;
+  }
+  else {
+      --secondsLeft;
+  }
+  setTime();
 }
